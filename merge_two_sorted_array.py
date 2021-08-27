d021f9a5410fa3fdb3420a1497c9242e2575f115
saurabhlengare15 <saurabhlengare15@gmail.com>
2021-08-27 11:37:29 +0530

    merge two sorted arrays of same and diff length

--- merge_two_sorted_array.py
+++ merge_two_sorted_array.py
@@ -1,17 +1,23 @@
-arr1 = [1,2,3,4,5,8,35]
-arr2 = [1,2,3,4,7,8,30]
+arr1 = [1,2,3,4,5]
+arr2 = [1,2,3,5,89,100,102]
 op = []
 first=0
 second=0
-while(first<len(arr1) and second<=len(arr2)):
+while(first<len(arr1) and second<len(arr2)):
     if(arr1[first]<=arr2[second]):
         op.append(arr1[first])
-        op.append(arr2[second])
+        first+=1
 
-    elif(arr1[first]>=arr2[second]):
+    else:
         op.append(arr2[second])
-        op.append(arr1[first])
+        second+=1
+
+while(first<len(arr1)):
+    op.append(arr1[first])
     first+=1
+
+while(second<len(arr2)):
+    op.append(arr2[second])
     second+=1
 
 print(op)
